f9cbe941c2a7cf13a3dea7d6fa41adc9b215d650
sanemyldz <sanemkavukoglu420@gmail.com>
2023-05-09 16:30:31 +0300

    Interactive Iterative Closest Point

---
+++ build/CMakeFiles/pcl_interactive_icp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl_interactive_icp.dir/interactive_icp.cpp.o"
+  "pcl_interactive_icp"
+  "pcl_interactive_icp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl_interactive_icp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
